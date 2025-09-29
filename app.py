b06b1b3e8edb7e8844894e9f3fbd55e2b0fabcbd
Jayz-R <johncarlos.recio@gmail.com>
2025-09-29 14:28:04 +0800

    Finalize edits before mergin

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
 
-@app.before_first_request
+@app.before_request
 def create_tables():
     db.create_all()
 
@@ -37,7 +37,7 @@ def edit(id):
         if request.method == 'POST':
              note.content = request.form['note']
              db.session.commit()
-            return redirect('/')
+             return redirect('/')
         return render_template('edit.html', note=note.content)
 
 if __name__ == '__main__':
